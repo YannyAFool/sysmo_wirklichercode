da8cc0ba84bddb2083dc323b3863e6840eab357d
YannyAFool <164160056+YannyAFool@users.noreply.github.com>
2024-03-20 13:26:35 +0100

    + "Leiser Alarm" => durch Drücken A+B => Kein Ton erklingt, wenn Alarm auslöst

--- main.ts
+++ main.ts
@@ -3,28 +3,62 @@ input.onButtonPressed(Button.A, function () {
     music.stopAllSounds()
     if (alarmAktiv == true) {
         alarmAktiv = false
+        playSound = false
     } else {
         alarmAktiv = true
+        playSound = true
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    music.stopAllSounds()
+    music.stopMelody(MelodyStopOptions.All)
+    if (playSound == true) {
+        playSound = false
+    }
+})
+/**
+ * Knopf B:
+ * 
+ *   == Ausschalten ertönender Alarm ==
+ * 
+ * - Schaltet einen ausgelösten Alarm aus
+ * 
+ * - Schaltet dabei alle Töne ab und setzt den Alarmstatus auf "Sleeping"
+ */
+/**
+ * Kopf A+B: ( = "Leiser Alarm") => Kein Ton, wenn Alarm !
+ * 
+ * == Schaltet nur das Ertönen eines Sounds ab ==
+ * 
+ * - anhalten aller Soundeffekte
+ * 
+ * - Setzt "playSound" auf "false", damit keine Töne gespielt werden (siehe Dauerschleife)
+ */
 input.onButtonPressed(Button.B, function () {
     alarmAusgeloest = 0
     music.stopAllSounds()
-    if (alarmPlaying == true) {
-        alarmPlaying = false
-        music.stopMelody(MelodyStopOptions.All)
-    }
+    music.stopMelody(MelodyStopOptions.All)
+    playSound = false
 })
 let value = 0
-let alarmPlaying = false
+let playSound = false
 let alarmAusgeloest = 0
 let alarmAktiv = false
 alarmAktiv = true
 alarmAusgeloest = 0
-alarmPlaying = false
+playSound = false
 bluetooth.startUartService()
 /**
  * <- Sendet nur Bluetooth, wenn Alarm scharf gestellt ist
+ */
+/**
+ * Knopf A: 
+ * 
+ *    ==Scharf schalten / Entschärfen der Alarmanlage==
+ * 
+ * - Stoppt ertönende Soundeffekte
+ * 
+ * - Deaktiviert / Aktiviert alle Alarme für die Zukunft
  */
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P1)
@@ -52,9 +86,8 @@ basic.forever(function () {
                 # . . . #
                 . # # # .
                 `)
-            if (alarmPlaying != true) {
+            if (playSound == true) {
                 music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.LoopingInBackground)
-                alarmPlaying = true
             }
         }
     } else {
