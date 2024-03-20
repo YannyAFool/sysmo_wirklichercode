10166f3ed928efbeed3974983addeac0ccccf206
YannyAFool <164160056+YannyAFool@users.noreply.github.com>
2024-03-20 10:54:15 +0100

    Beim Start und Dauerhaft - Funktionen geadded

---
+++ main.py
@@ -0,0 +1,7 @@
+alarmAktiv = True
+alarmAusgeloest = 0
+alarmPlaying = False
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+let value = 0
+let alarmAktiv = true
+let alarmAusgeloest = 0
+let alarmPlaying = false
+bluetooth.startUartService()
+/**
+ * <- Sendet nur Bluetooth, wenn Alarm scharf gestellt ist
+ */
 basic.forever(function () {
-	
+    value = pins.analogReadPin(AnalogPin.P1)
+    if (value != 0) {
+        alarmAusgeloest = 1
+    }
+    if (alarmAusgeloest == 0) {
+        bluetooth.uartWriteString("nein")
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        if (alarmAktiv == true) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            bluetooth.uartWriteString("ja")
+            if (alarmPlaying != true) {
+                music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.LoopingInBackground)
+                alarmPlaying = true
+            }
+        }
+    }
+    basic.pause(100)
 })
